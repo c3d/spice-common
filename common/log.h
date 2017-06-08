11d727b8470024e691c3db355a62985d22461dfe
Christophe de Dinechin <dinechin@redhat.com>
2017-06-08 13:35:42 +0200

    Add TWEAK(...) to identify where we access tweaks

--- common/log.h
+++ common/log.h
@@ -111,6 +111,7 @@ void spice_log(const char *log_domain,
 
 /* Tracing facilities */
 #define TRACE(name)     (spice_traces.name)
+#define TWEAK(name)     (spice_traces.name)
 #define IFTRACE(name)   if (G_UNLIKELY(TRACE(name)))
 #define spice_trace(name, format, ...)                          \
     G_STMT_START {                                              \
